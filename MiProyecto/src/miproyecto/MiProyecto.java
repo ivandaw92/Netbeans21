2532b6e915117d8ecfe9728bdb6d82fcafe19aa1
admin <admin@Win7_1TB>
2021-01-22 12:59:53 +0300

    He hecho otro cambio

--- MiProyecto/src/miproyecto/MiProyecto.java
+++ MiProyecto/src/miproyecto/MiProyecto.java
@@ -16,7 +16,25 @@ public class MiProyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+    int numeros[]=new int[10];    
     System.out.println("Probando github");
+    for(int i=0;i<numeros.length;i++){
+        numeros[i]=5;
+    }
+    
+    /*
+    Guardar datos
+    Clone: de github (main o master) a local (head)
+    Push: de local a github
+    
+    Diff para ver los cambios
+    
+    Desde team o click derecho/git/ commit para guardar cambios
+    con comentarios de lo nuevo.
+    
+    Y en git/remote/clone para guardarlo en github
+    */
     
     }
     
